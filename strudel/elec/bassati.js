323d334c1b0fea530e08e73e85224de9abaa9002
sangarshanan <sangarshanan@gmail.com>
2026-06-17 19:37:05 +0200

    stuff

--- strudel/elec/bassati.js
+++ strudel/elec/bassati.js
@@ -9,10 +9,22 @@ drone: s("square")
   .detune(1)
 
 white: s("white!8")
-  .decay(0.08)
-  .almostNever(ply("2 | 4"))
-  .gain(0.1)
+  .decay(0.09)
+  .delay(0.6)
+  .almostNever(ply("2"))
+  .gain(0.4)
 
-kick: sound("bd*2").bank("RolandTR909")
+kick: sound("bd*4").gain(0.5)
 
-saw: sound("supersaw").detune(1).rel(5).beat(2, 32).slow(4).fm(2).phaser(0.2)
+saw: sound("supersaw").detune(1).rel(5).beat(2, 32).slow(4).fm(2)
+
+_riser: riser(16, 8, (x, rise) => x.s("supersaw").detune(rise))
+
+vibes :s("sawtooth,tri,saw,sine")
+  .gain(saw.rev().range(3,1))
+  .lpf(saw.rangex(200,2200).slow(8))
+  .beat("3,7,9",16).vib(2,.5)
+  .clip(.7)
+  .delay(1/2)
+  .diode("1:.5")
+  .acidenv(slider(0.773))
---
+++ strudel/set/2.js
@@ -0,0 +1,30 @@
+// just techno
+
+setCps(0.5)
+
+let scale = "Eb:dorian"
+
+$: s("bd*4").duck("2").duckattack(.07).decay("<.5>").fm(100).fmh(10)
+
+$:  s("[sd]*2").decay(.1).delay(1).bpenv(0.5).lpf(1000)
+
+// you can hh*4 to make it hard and *2 for soft
+$:  s("9000_hh*4").decay(.1).delay(1).lpf(3000)
+
+
+_$: n(irand(32).add(23)).sound("saw").room(1).decay(0.5).seg(16).acidenv(0.5).gain(1.5).orbit(2)
+
+_$: n("[ 0 -5]!1/2 | [0 3]!1/2")
+.scale(scale)
+.s("gm_electric_guitar_jazz").distort(0.5)
+
+
+$: n("[ 3 -2]!1/2 | [3 8]!1/2")
+.scale(scale)
+.s("gm_trombone").orbit(2)
+
+_$: n(irand(8).add(12).scale("major")
+   .struct("<0@.25 0.125> <1@2 1 1@.5> 1@.6 1@1.9 1 <1@1 1 1@2>"))
+   .scale(scale)
+   .s("piano")
+  .orbit(2)
